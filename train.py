86fe1e0f9f6046615ae8f00852a421a4fc33f1ec
yamaru12345 <65715903+yamaru12345@users.noreply.github.com>
2020-11-06 10:13:38 +0900

    Update train.py

--- train.py
+++ train.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
             for t, m, s in zip(warp_image, mean, std):
                 t.mul_(s).add_(m)
             plt.imshow((warp_image.permute(1, 2, 0).cpu().numpy() * 255).astype(np.uint8))
-            plt.show()
+            plt.savefig(f"./warp_image_epoch{epoch}.png")
             
         # Training one epoch
         train_loss = train_epoch(model,
