e9624eb8afa93eda6b1a1aafb5eeb3cc3257fad1
yamaru12345 <65715903+yamaru12345@users.noreply.github.com>
2020-11-11 16:39:02 +0900

    Update train.py

--- train.py
+++ train.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     #model = nn.DataParallel(model)
     model = model.to(device)
     if args.use_pretrained:
-        model.load_state_dict(torch.load('/content/drive/My Drive/DGC-Net/snapshots/2020_11_09_08_12/epoch_6.pth')['state_dict'])
+        model.load_state_dict(torch.load('/content/drive/My Drive/DGC-Net/snapshots/2020_11_10_06_48/epoch_6.pth')['state_dict'])
 
     # Optimizer
     optimizer = \
